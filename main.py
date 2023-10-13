2e4d1b2e4fc84d920b6c8cfb9ede1ea4167625c7
Tahir-Ryasnov <107679690+Tahir-Ryasnov@users.noreply.github.com>
2023-10-14 01:03:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-import requests
-from pprint import pprint
+from database.database import create_tables
+from tg_bot.tg_bot import bot
 
-"""locations/search (Deprecated)"""
-
-url = "https://hotels4.p.rapidapi.com/locations/search"
-
-querystring = {"query":"grozny","locale":"en_US"}
-
-headers = {
-	"X-RapidAPI-Key": "e63af22e92msh9f446d4dfc9ba1ap1e570ajsn083b4c33848b",
-	"X-RapidAPI-Host": "hotels4.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-response = response.json()
-dest_id = response['suggestions'][1]['entities'][0]['destinationId']
-# 757810592
-
-"""properties/list (Deprecated)"""
-url = "https://hotels4.p.rapidapi.com/properties/list"
-
-querystring = {"destinationId":dest_id,"pageNumber":"1","pageSize":"5","checkIn":"2023-11-25","checkOut":"2023-11-27","adults1":"1","sortOrder":"PRICE","locale":"ru_RU","currency":"USD"}
-
-headers = {
-	"X-RapidAPI-Key": "e63af22e92msh9f446d4dfc9ba1ap1e570ajsn083b4c33848b",
-	"X-RapidAPI-Host": "hotels4.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-pprint(response)
+if __name__ == '__main__':
+    create_tables()
+    bot.infinity_polling()
